f3471931241a53f7b8492b195aab9d163d5998ef
Admir Schaurich <admirsch@gmail.com>
2023-11-05 21:53:24 -0300

    Add endpoint mt-to-km

--- Java/api-restfull-with-swagger/src/main/java/br/com/schaus/convert/convert/ConvertController.java
+++ Java/api-restfull-with-swagger/src/main/java/br/com/schaus/convert/convert/ConvertController.java
@@ -1,5 +1,6 @@
 package br.com.schaus.convert.convert;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,18 @@ import java.text.DecimalFormat;
 @RequestMapping("/convert")
 public class ConvertController {
     @GetMapping("/km-to-mt/{km}")
-    public String getHelloWorld(@PathVariable double km){
+    @Operation(summary = "Conversão de quilômetros para metros", description = "Essa função executa a conversão de quilômetros para metros.")
+    public String getKmToMt(@PathVariable double km){
         double meters = km * 1000;
         DecimalFormat df = new DecimalFormat("#.##");
         return km + " km, é equivalente a " + df.format(meters) + " mt";
+    }
+
+    @GetMapping("/mt-to-km/{mt}")
+    @Operation(summary = "Conversão de metros para quilômetros", description = "Essa função executa a conversão de metros para quilômetros.")
+    public String getMtToKm(@PathVariable double mt){
+        double km = mt / 1000;
+        DecimalFormat df = new DecimalFormat("#.##");
+        return mt + " mt, é equivalente a " + df.format(km) + " km";
     }
 }
